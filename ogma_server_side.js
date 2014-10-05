bf4bd033bedef4e91a1c91ec3f5b1d810e251b10
John Roach <johnroach1985@gmail.com>
2014-10-05 12:49:05 +0300

    - Started writing a minimalistic website for choosing nodes...

--- ogma_server_side.js
+++ ogma_server_side.js
@@ -1,3 +1,61 @@
-/**
- * Created by john on 10/5/14.
- */
+
+var path = require('path');
+var fs = require('fs');
+
+var io = require("socket.io").listen(8100);
+
+var node_ip="";
+
+
+var app = require('http').createServer(function (request, response) {
+    console.log('request starting...');
+
+    var filePath = '.' + request.url;
+    if (filePath == './')
+        filePath = './index.html';
+
+    var extname = path.extname(filePath);
+    var contentType = 'text/html';
+    switch (extname) {
+        case '.js':
+            contentType = 'text/javascript';
+            break;
+        case '.css':
+            contentType = 'text/css';
+            break;
+    }
+
+    fs.exists(filePath, function(exists) {
+
+        if (exists) {
+            fs.readFile(filePath, function(error, content) {
+                if (error) {
+                    response.writeHead(500);
+                    response.end();
+                }
+                else {
+                    response.writeHead(200, { 'Content-Type': contentType });
+                    response.end(content, 'utf-8');
+                }
+            });
+        }
+        else {
+            response.writeHead(404);
+            response.end();
+        }
+    });
+
+});
+
+io.sockets.on("connection",function(socket){
+    // Display a connected message
+    console.log("Server-Client Connected!");
+
+    // When we receive a message...
+    socket.on("ip",function(data){
+        node_ip=data;
+        socket.broadcast.emit('node_ip_info_received', node_ip);
+    });
+
+
+});
